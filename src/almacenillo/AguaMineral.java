78a15dae9fb66e013cf3c148a57dae0a4e53ded6
Nestorsoiza <nestor.soiza83@gmail.com>
2020-12-08 22:07:19 +0100

    hechos las 3 primeros atributos

--- src/almacenillo/AguaMineral.java
+++ src/almacenillo/AguaMineral.java
@@ -1,5 +1,25 @@
 package almacenillo;
 
-public class AguaMineral {
+public class AguaMineral extends Bebida {
+	private String origen;
 
+	public AguaMineral(double cantidad, double precio, String marca, String origen) {
+		super(cantidad, precio, marca);
+		this.origen = origen;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	@Override
+	public String toString() {
+		return "AguaMineral con origen en " + origen + super.toString();
+	}
+	
+	
 }
--- src/almacenillo/BebidasAzucaradas.java
+++ src/almacenillo/BebidasAzucaradas.java
@@ -1,5 +1,46 @@
 package almacenillo;
 
-public class BebidasAzucaradas {
+public class BebidasAzucaradas extends Bebida {
+	private double porcentajeAzucar;
+	private boolean promocion;
+	
+	public BebidasAzucaradas(double cantidad, double precio, String marca, double porcentajeAzucar,
+			boolean promocion) {
+		super(cantidad, precio, marca);
+		this.porcentajeAzucar = porcentajeAzucar;
+		this.promocion = promocion;
+	}
 
+	public double getPorcentajeAzucar() {
+		return porcentajeAzucar;
+	}
+
+	public void setPorcentajeAzucar(double porcentajeAzucar) {
+		this.porcentajeAzucar = porcentajeAzucar;
+	}
+
+	public boolean isPromocion() {
+		return promocion;
+	}
+
+	public void setPromocion(boolean promocion) {
+		this.promocion = promocion;
+	}
+
+	@Override
+	public double getPrecio() {
+		double precioDescuento = 0; 
+		if (isPromocion()) {
+		 precioDescuento += getPrecio() * 0.9;
+		}
+		return super.getPrecio();
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " BebidasAzucaradas [porcentajeAzucar= " + porcentajeAzucar + ", promocion= " + promocion 
+				+ ", toString()= " +  "]";
+	}
+
+	
 }
